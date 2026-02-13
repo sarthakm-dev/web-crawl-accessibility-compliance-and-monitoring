b9cc69f1c7a351a4843d4ab646d143b12415229b
Sarthak Mishra <sarthakm@mindfiresolutions.com>
2026-02-13 15:04:49 +0530

    chore: update eslint and prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,38 @@
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import tsParser from "@typescript-eslint/parser";
+import globals from "globals";
 
-export default defineConfig([
-  globalIgnores(['dist']),
-
+export default [
   {
-    files: ['*/.{ts}'],
-
+    ignores: [
+      "**/node_modules/**",
+      "**/test/**",
+      "**/dist/**",
+      "**/*.d.ts",
+      "**/documentation/**",
+    ],
+  },
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
     languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+      },
       globals: {
-        ...globals.browser,
-        ...globals.node,
+        ...globals.builtin,
       },
     },
-
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-    },
-
     rules: {
-      semi: ['error', 'always'],
-      quotes: ['error', 'single'],
-      indent: ['error', 2],
-      curly: ['error', 'all'],
-      eqeqeq: ['error', 'always'],
-      'no-console': 'warn',
-      complexity: ['error', 10],
-      'max-depth': ['error', 3],
-      'max-lines-per-function': ['warn', 30],
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/explicit-function-return-type': 'warn',
+      "no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "none",
+          ignoreRestSiblings: false,
+        },
+      ],
+      // Add more rules as needed
     },
   },
-]);
+];
