13b35f8626f201b4e9aa60c3915fdab38072cdbd
Sarthak Mishra <sarthakm@mindfiresolutions.com>
2026-02-17 17:15:27 +0530

    feat(api): add cors in api-gateway

--- apps/api-gateway/src/server.ts
+++ apps/api-gateway/src/server.ts
@@ -3,11 +3,14 @@ import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./docs/openapi.json";
 import authRoutes from "./routes/auth.routes";
 import dotenv from 'dotenv';
-
+import cors from "cors";
 dotenv.config();
 
 const app = express();
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 app.use("/auth",authRoutes)
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
