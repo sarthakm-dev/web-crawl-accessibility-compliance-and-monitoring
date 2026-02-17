f834936e2b510f65dca2c4612d1fd5698c25801f
Sarthak Mishra <sarthakm@mindfiresolutions.com>
2026-02-17 17:30:38 +0530

    feat(core): add shared rbac validation

--- apps/auth-service/src/middlewares/auth.middleware.ts
+++ apps/auth-service/src/middlewares/auth.middleware.ts
@@ -21,16 +21,3 @@ export const authenticate = (req: AuthRequest, res: Response, next: NextFunction
     return res.status(401).json({ error: 'Invalid token' });
   }
 };
-
-export const authorize = (requiredPermission: string) => {
-  //Ex router.post("/sites",authenticate,authorize("site:create"),SiteController.create);
-  return (req: any, res: any, next: any) => {
-    const userPermissions = req.user?.permissions || [];
-
-    if (!userPermissions.includes(requiredPermission)) {
-      return res.status(403).json({ error: 'Forbidden' });
-    }
-
-    next();
-  };
-};
