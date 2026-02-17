76bcd2f4e79f48f5309654859a3fbcdf9bcad5c1
Sarthak Mishra <sarthakm@mindfiresolutions.com>
2026-02-17 17:37:47 +0530

    feat(api):add morgan for HTTP request logging

--- apps/api-gateway/src/server.ts
+++ apps/api-gateway/src/server.ts
@@ -3,6 +3,7 @@ import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./docs/openapi.json";
 import authRoutes from "./routes/auth.routes";
 import dotenv from 'dotenv';
+import logger from "morgan";
 import cors from "cors";
 dotenv.config();
 
@@ -11,6 +12,7 @@ app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
+app.use(logger('dev'));
 app.use(express.json());
 app.use("/auth",authRoutes)
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
