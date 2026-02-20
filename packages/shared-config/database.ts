a54b643f957a2a6a44f29bd95950786d383dadbb
Sarthak Mishra <sarthakm@mindfiresolutions.com>
2026-02-20 14:21:24 +0530

    fix(db): change postgres host

--- packages/shared-config/database.ts
+++ packages/shared-config/database.ts
@@ -1,20 +1,20 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
 declare global {
-   // eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line no-unused-vars
   var _sequelize_: Sequelize | undefined;
 }
 
 export const sequelize =
   global._sequelize_ ??
   new Sequelize(
-    process.env.DB_NAME || "webcrawl",
-    process.env.DB_USER || "postgres",
-    process.env.DB_PASSWORD || "postgres",
+    process.env.DB_NAME || 'webcrawl',
+    process.env.DB_USER || 'postgres',
+    process.env.DB_PASSWORD || 'postgres',
     {
-      host: process.env.DB_HOST || "localhost",
-      port: Number(process.env.DB_PORT) || 5433,
-      dialect: "postgres",
+      host: process.env.DB_HOST || 'postgres',
+      port: Number(process.env.DB_PORT) || 5432,
+      dialect: 'postgres',
       logging: false,
     }
   );
