3054cdb82f02f67e091d33cf8fda82a7070891ed
Sarthak Mishra <sarthakm@mindfiresolutions.com>
2026-02-23 13:03:55 +0530

    feat(api): add create site route to api-gateway

--- apps/api-gateway/src/controllers/auth.controller.ts
+++ apps/api-gateway/src/controllers/auth.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import axios from 'axios';
 
 const AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL;
-
+const CRAWL_MANAGER_URL = process.env.CRAWL_MANAGER_URL;
 export const AuthController = {
   async signup(req: Request, res: Response) {
     try {
@@ -126,6 +126,16 @@ export const AuthController = {
         `${AUTH_SERVICE_URL}/verify-otp`,
         req.body
       );
+      return res.status(200).json(response.data);
+    } catch (err: any) {
+      return res.status(err.response?.status || 500).json({
+        error: err.response?.data?.error || 'Password Reset Failed',
+      });
+    }
+  },
+  async createSite(req: Request, res: Response) {
+    try {
+      const response = await axios.post(`${CRAWL_MANAGER_URL}/sites`, req.body);
       return res.status(200).json(response.data);
     } catch (err: any) {
       return res.status(err.response?.status || 500).json({
