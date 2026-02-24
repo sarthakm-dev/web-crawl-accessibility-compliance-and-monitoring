ebc0bd8fd0e6949f56142ae309ebde1a1a981630
Sarthak Mishra <sarthakm@mindfiresolutions.com>
2026-02-24 11:01:24 +0530

    feat(api): add rbac middleware for crawl-manager

---
+++ apps/crawl-manager/src/middleware/crawl.middleware.ts
@@ -0,0 +1,29 @@
+import { Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { AuthRequest } from '@packages/shared-types/auth.types';
+
+export const authenticate = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies.accessToken;
+  console.log(req.cookies);
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+      userId: string;
+      roles: string[];
+      permissions: string[];
+    };
+    req.userId = decoded.userId;
+    req.roles = decoded.roles;
+    req.permissions = decoded.permissions;
+
+    next();
+  } catch {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+};
