94c22fbd0055d351e20e60707b8214a7c18fe6ad
Sarthak Mishra <sarthakm@mindfiresolutions.com>
2026-03-16 21:43:30 +0530

    fix(core): update package.json

--- packages/shared-types/index.ts
+++ packages/shared-types/index.ts
@@ -1,6 +1,6 @@
 export * from './auth.types';
 export * from './crawl-job.types';
-export * from './express';
+export * from './express.d';
 export * from './issue.types';
 export * from './site.types';
 export * from './user.types';
